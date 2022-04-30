5348617426da7e82a8b98ef5701c0d40a9fffa15
Jacob Castor <jcastor@uncc.edu>
2022-04-30 17:08:25 -0400

    Changed JS file

--- SWEFinal.js
+++ SWEFinal.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
+
+    var json = (function () {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "SWEFinal.json",
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })();
+
     $("a").each(function() {
+        var currentCounty = this;
         $(this).hover(function(){
             $("#countyName").text("Selected County:" + $(this).attr("title"));
         });
         $(this).click(function(){
             $("#countyTable").text($(this).attr("title"));
 
-            var json = (function () {
-                var json = null;
-                $.ajax({
-                    'async': false,
-                    'global': false,
-                    'url': "SWEFinal.json",
-                    'dataType': "json",
-                    'success': function (data) {
-                        json = data;
-                    }
-                });
-                return json;
-            })();
-
             $.each(json.counties.county_name, function(i, v) {
-                if (v.county_name == this) {
+                if (v.county_name == currentCounty) {
                     alert(v.averageRent);
                     return;
                 }
